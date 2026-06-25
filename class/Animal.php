31a1697dd508960561c6d16c6a94482ba2c3ae0f
Juxo28 <julianpalaciol2804@gmail.com>
2026-06-25 17:29:36 -0500

    Creacion objeto Animal

---
+++ class/Animal.php
@@ -0,0 +1,14 @@
+<?php
+
+class Perro
+{
+    public function ladrar ()
+    {
+        echo "Woof Woof Woof";
+    }
+    public function dormir ()
+    {
+        echo "ZzzzZzzzZ";
+    }
+}
+
--- public/index.php
+++ public/index.php
@@ -6,3 +6,11 @@ $julian = new Persona();
 
 $julian->saludar();
 $julian->jugar();
+echo "<br><br>";
+
+require_once "../class/Animal.php";
+
+$mascota = new Perro ();
+$mascota->ladrar();
+echo "<br>";
+$mascota->dormir();
